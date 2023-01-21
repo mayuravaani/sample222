b356e53b9478af8d94a9f56e6d643008672593a8
mayuravaani <mmayuravaani@gmail.com>
2023-01-21 11:30:26 +0530

    from sub branch

--- Sum.java
+++ Sum.java
@@ -3,6 +3,7 @@ class Sum {
         int a = 3;
         int b = 4;
         System.out.println(a+b);
+        System.out.println(a-b);
         // this is from local machine 1
         //this is a summation operation
         //from machine 2
@@ -11,5 +12,6 @@ class Sum {
         //task 2
         // i am editing this from branch sub
         // i am changing 13th line from sub branch
+        
     }
 }
